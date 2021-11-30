2c2863177011878ad50ae0c85773349bb0f0d347
Meryem Sevval Ar <76441991+meryemsevvalar@users.noreply.github.com>
2021-11-30 17:37:29 +0300

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,19 @@
+// setInterval(() => {
+//     console.log("hello");
+// }, 1000);
+
+import fetch from 'node-fetch';
+
+fetch ("https://jsonplaceholder.typicode.com/users")
+.then((data) => data.json())
+.then((users) => {
+    console.log("Users yüklendi!", users);
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then ((data) => data.json())
+.then ((posts) => console.log(posts, "Posts Yüklendi!"));
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+.then ((data) => data.json())
+.then ((post) => console.log(post, "Post Yüklendi!"));
+});
